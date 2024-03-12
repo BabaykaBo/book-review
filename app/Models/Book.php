235f474ff1a8be679e842b209e461b0d8cd77583
Oleh <zaaqq135@gmail.com>
2024-03-12 23:46:06 +0200

    created new scope query methods for book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -21,4 +21,34 @@ class Book extends Model
     {
         return $query->where('title', 'LIKE', "%$title%");
     }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+            ])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
+    {
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+            ], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function dateRangeFilter(Builder $q, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $q->where('created_at', '>=', $from);
+    } elseif (!$from && $to) {
+            $q->where('created_at', '<=', $to);
+    } elseif ($from && $to){
+            $q->whereBetween('created_at', [$from, $to]);
+    }
+    }
 }
