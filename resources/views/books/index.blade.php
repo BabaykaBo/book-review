67e03096118305c28d88d8de5699c02f315a465a
Oleh <zaaqq135@gmail.com>
2024-03-18 23:39:58 +0200

    added filters into index view

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -1,9 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="">Books Reviews</h1>
+    <h1 class="mb-10 text-2x1">Books Reviews</h1>
 
-    <form action="mb-10 text-2x1"></form>
+    <form class="mb-5 flex item-center" method="GET" action="{{ route('books.index') }}">
+        <input class="input mb-5 mr-5" type="text" name="title" id="title" placeholder="Search by title"
+            value="{{ request('title') }}">
+        <input type="hidden" name="filter" value="{{ request('filter') }}">
+        <button class="btn mr-5" type="submit">Search</button>
+        <a class="btn" href="{{ route('books.index') }}">Clear</a>
+    </form>
+
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+                '' => 'Latest',
+                'popular_last_month' => 'Popular Last Month',
+                'popular_last_6months' => 'Popular Last 6 Months',
+                'highest_rated_last_month' => 'Highest Rated Last Month',
+                'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+            ];
+        @endphp
+
+        @foreach ($filters as $key => $filter)
+            <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
+                class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}">
+                {{ $filter }}
+            </a>
+        @endforeach
+    </div>
 
     <ul>
         @forelse ($books as $book)
